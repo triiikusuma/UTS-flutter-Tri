bdef7cdc5b1ddc2d44d7db6f82da7106b643b849
triiikusuma <trikusumaa06@gmail.com>
2024-04-04 15:45:55 +0800

    Mengganti page dari intro menjadi home page, membuat folder untuk masing" card, menambahkan assets untuk image lokal, dan mencoba menambahkan fitur dark mode (tetapi sat dijalankan masih debug)

---
+++ lib/card/card1.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatefulWidget {
+  const Card1({super.key});
+  
+  @override
+  State<Card1> createState() => _Card1();
+}
+
+class _Card1 extends State<Card1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("P R O F I L E  M A H A S I S W A",
+        style: TextStyle(
+        fontWeight: FontWeight.bold
+        ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        ),
+body:ListView(
+        children: <Widget> [
+          Padding(
+            padding: const EdgeInsets.all(8.6),
+            child: Image.network("https://t3.ftcdn.net/jpg/06/87/80/96/360_F_687809694_8wvi3jo9ANyjZPLEVUD2ufRPiLHpR1ad.jpg"),
+          ),
+          const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(Icons.home),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(Icons.school),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(Icons.settings),
+          ),
+          ],
+          ),
+
+        const ListTile(
+          title: Text("Tri Kusuma",),
+          subtitle: Text("Nama"),
+        ),
+        const ListTile(
+          title: Text("Fakultas Teknik dan Informatika"),
+          subtitle: Text("Fakultas"),
+        ),
+        const ListTile(
+          title: Text("Teknologi Informasi"),
+          subtitle: Text("Program Studi"),
+        ),
+        const ListTile(
+          title: Text("421313252"),
+          subtitle: Text("NIM"),
+        ),
+        const ListTile(
+          title: Text("Jalan pratama No.33 Wn"),
+          subtitle: Text("Alamat"),
+        ),
+        const ListTile(
+          title: Text("Teknologi Informasi (TI) merujuk pada penggunaan komputer dan perangkat lunak untuk mengumpulkan, menyimpan, mengolah, dan menyebarkan informasi. Ini mencakup berbagai macam teknologi dan sistem yang digunakan untuk mengelola dan mengirimkan data dalam berbagai bentuk. Teknologi Informasi juga mencakup infrastruktur teknologi yang mendukung proses ini, seperti jaringan komputer, server, dan perangkat keras dan perangkat lunak lainnya."),
+        )
+        ]
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_uts/card/card1.dart';
+import 'package:flutter_application_uts/card/card2.dart';
+import 'package:flutter_application_uts/card/card3.dart';
+import 'package:flutter_application_uts/card/card4.dart';
+import 'package:flutter_application_uts/card/card5.dart';
 import 'package:flutter_application_uts/pages/home_page.dart';
-import 'package:flutter_application_uts/pages/intro_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +18,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const IntroPage(),
+      home: const HomePage(),
       routes: {
+        '/card1':(context) => const Card1(),
+        '/card2':(context) => const Card2(),
+        '/card3':(context) => const Card3(),
+        '/card4':(context) => const Card4(),
+        '/card5':(context) => const Card5(),
         '/homepage':(context) => const HomePage(),
-        '/intropage':(context) => const IntroPage(),
       },
     );
   }
